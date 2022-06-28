be5e6a062cd4814feec5a1cf5b86363ebf016ee3
Leechael Yim <yanleech@gmail.com>
2022-06-28 20:59:08 +0800

    chore: helper types.

---
+++ @types/helper.d.ts
@@ -0,0 +1,10 @@
+type ArrayLengthMutationKeys = 'splice' | 'push' | 'pop' | 'shift' | 'unshift' | number
+type ArrayItems<T extends Array<any>> = T extends Array<infer TItems> ? TItems : never
+type FixedLengthArray<T extends any[]> =
+  Pick<T, Exclude<keyof T, ArrayLengthMutationKeys>>
+  & { [Symbol.iterator]: () => IterableIterator< ArrayItems<T> > }
+
+interface Pairs<T> extends Array<any> {
+  0: string;
+  1: T;
+}
