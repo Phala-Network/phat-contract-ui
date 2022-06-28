309061aac307958fc416390c605fabdcbcc7a884
Leechael Yim <yanleech@gmail.com>
2022-06-29 04:21:05 +0800

    imp: landing page UX

--- src/pages/contract-list-page.tsx
+++ src/pages/contract-list-page.tsx
@@ -5,9 +5,10 @@ import React, { Suspense, useState } from 'react'
 import tw from 'twin.macro'
 import { useAtomValue, useUpdateAtom } from 'jotai/utils'
 import { Box, Button, ButtonGroup, Stack, Skeleton } from '@chakra-ui/react'
-import { Link } from '@tanstack/react-location'
-import { AiOutlineReload } from 'react-icons/ai'
+import { Link, useNavigate } from '@tanstack/react-location'
+import { AiOutlineReload, AiOutlinePlus } from 'react-icons/ai'
 
+import { lastSelectedAccountAddressAtom, connectionDetailModalVisibleAtom } from '@/features/account/atoms'
 import { hasConnectedAtom, availableContractsAtom } from '@/features/chain/atoms'
 
 const ContractListSkeleton = () => (
@@ -50,20 +51,71 @@ const ContractCell: FC<LocalContractInfo> = ({ contractId, metadata, savedAt }) 
 const ContractList = () => {
   const hasConnected = useAtomValue(hasConnectedAtom)
   const contracts = useAtomValue(availableContractsAtom)
+  const lastSelectedAccountAddress = useAtomValue(lastSelectedAccountAddressAtom)
+  const setConnectionDetailModalVisible = useUpdateAtom(connectionDetailModalVisibleAtom)
+  const navigate = useNavigate()
   if (!hasConnected) {
     return <ContractListSkeleton />
   }
   if (contracts.length === 0) {
-    return null
+    return (
+      <div tw="bg-black py-6 min-w-full text-center">
+        <svg
+          tw="mx-auto h-12 w-12 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            vectorEffect="non-scaling-stroke"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+          />
+        </svg>
+        <h3 tw="mt-2 text-sm font-medium text-gray-400">No Contract</h3>
+        <p tw="mt-1 text-sm text-gray-500">Get started by uploading a new fat contract.</p>
+        <div tw="mt-6 mb-4">
+          <Button
+            tw="
+            inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md transition-colors
+            text-black bg-phalaDark-500
+            hover:bg-gray-900 hover:text-phalaDark-500
+            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-phala-500
+            "
+            onClick={() => {
+              if (!lastSelectedAccountAddress) {
+                setConnectionDetailModalVisible(true)
+              } else {
+                navigate({ to: '/contracts/add' })
+              }
+            }}
+          >
+            <AiOutlinePlus tw="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+            {lastSelectedAccountAddress ? 'Upload' : 'Sign In'}
+          </Button>
+        </div>
+      </div>
+    )
   }
   return (
-    <div tw="mt-2 mb-4 bg-black p-4 max-w-4xl min-w-full">
-      {contracts.map(([key, info]) => (
-        <div key={key}>
-          <ContractCell {...info} />
-        </div>
-      ))}
-    </div>
+    <>
+      <ButtonGroup>
+        <Link to="/contracts/add">
+          <Button bg="black" borderRadius={0} as="span">Add New Contract</Button>
+        </Link>
+        <ReloadButton />
+      </ButtonGroup>
+      <div tw="mt-2 mb-4 bg-black p-4 max-w-4xl min-w-full">
+        {contracts.map(([key, info]) => (
+          <div key={key}>
+            <ContractCell {...info} />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
@@ -88,15 +140,20 @@ const ReloadButton = () => {
 
 const ContractListPage = () => {
   return (
-    <div tw="grid grid-cols-12 w-full gap-0">
-      {/* <div tw="col-span-3 order-2 px-2"></div> */}
-      <div tw="col-span-12 order-1 px-2">
-        <ButtonGroup>
-          <Link to="/contracts/add">
-            <Button bg="black" borderRadius={0} as="span">Add New Contract</Button>
-          </Link>
-          <ReloadButton />
-        </ButtonGroup>
+    <div tw="grid grid-cols-12 w-full gap-2">
+      <div tw="col-span-3 order-2 pl-6">
+        <div tw="flex flex-col gap-4">
+          <Button w="full" as="a" href="https://wiki.phala.network/" target="_blank">Wiki</Button>
+          <Button w="full" as="a" href="https://discord.gg/phala" target="_blank">Discord</Button>
+          <Button w="full" as="a" href="https://github.com/Phala-Network/awesome-fat-contracts" target="_blank">
+            Awesome Fat Contracts
+          </Button>
+          <Button w="full" as="a" href="https://github.com/Phala-Network/oracle-workshop" target="_blank">
+            Oracle Workshop
+          </Button>
+        </div>
+      </div>
+      <div tw="col-span-9 order-1">
         <Suspense fallback={<ContractListSkeleton />}>
           <ContractList />
         </Suspense>
