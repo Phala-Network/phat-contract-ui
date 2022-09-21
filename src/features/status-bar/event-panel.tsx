9a348face25fc036a227dbc0dcbe69d976d1b16b
Leechael Yim <leechael@phala.network>
2022-09-21 08:48:57 +0800

    fix: Event object data structure change UI crash.

--- src/features/status-bar/event-panel.tsx
+++ src/features/status-bar/event-panel.tsx
@@ -19,43 +19,27 @@ const EventPanel = () => {
             Empty.
           </div>
         )}
-        {events.map((event, index) => (
-          <article key={index} tw="flex-grow bg-gray-900 border border-solid border-gray-700 rounded-sm px-4 pt-2 pb-3">
-            <div tw="mb-2">
-              <Badge borderRadius='full' px='2' colorScheme='phala' mr="2">{event.section}</Badge>
-              <Badge borderRadius='full' px='2' colorScheme='phalaDark'>{event.method}</Badge>
-            </div>
-            {event.section === 'balances' && (
-              <div tw="px-1 text-xs font-mono">
-                <p tw="my-2">{event.data[0] as unknown as string}</p>
-                <p tw="my-2">{event.data[1] as unknown as string}</p>
+        {events.map((event, index) => {
+          const pairs = R.toPairs(event.data)
+          return (
+            <article key={index} tw="flex-grow bg-gray-900 border border-solid border-gray-700 rounded-sm px-4 pt-2 pb-3">
+              <div>
+                <Badge borderRadius='full' px='2' colorScheme='phala' mr="2">{event.section}</Badge>
+                <Badge borderRadius='full' px='2' colorScheme='phalaDark'>{event.method}</Badge>
               </div>
-            )}
-            {event.section === 'treasury' && (
-              <div tw="px-1 text-xs font-mono">
-                <p tw="my-2">{event.data[0] as unknown as string}</p>
-              </div>
-            )}
-            {event.section === 'phalaFatContracts' && event.method === 'Instantiating' && (
-              <div tw="px-1 text-xs font-mono">
-                {event.data.map((data, index) => (
-                  <p tw="my-2" key={index}>{data as unknown as string}</p>
-                ))}
-              </div>
-            )}
-            {event.section === 'system' && event.data.map((rec: object, idx) => (
-              <div key={idx} tw="px-1 text-sm font-mono">
-                {R.toPairs(rec).map(([key, value]: [string, string]) => (
-                  (typeof value === 'string') ? (
-                    <div tw="my-2" key={key}>{`${key}: ${value}`}</div>
-                  ) : (
-                    <div tw="my-2" key={key}>{`${key}: ${JSON.stringify(value)}`}</div>
-                  )
-                ))}
-              </div>
-            ))}
-          </article>
-        ))}
+              {(pairs.length > 0) && (
+                <dl tw="mt-2 px-1 text-xs font-mono">
+                  {pairs.map(([key, value]) => (
+                    <div tw="my-2 flex flex-row" key={key}>
+                      <dt tw="mr-2">{key}:</dt>
+                      <dd>{(typeof value === 'string') ? value : JSON.stringify(value)}</dd>
+                    </div>
+                  ))}
+                </dl>
+              )}
+            </article>
+          )
+        })}
       </div>
     </div>
   )
