ff13eeec479f53551870d1798c0544b268dd4494
Leechael Yim <yanleech@gmail.com>
2022-10-29 12:31:49 +0800

    feat: save last selected endpoint into local storage.

--- src/atoms/endpointsAtom.ts
+++ src/atoms/endpointsAtom.ts
@@ -1,4 +1,5 @@
-import {atomWithReset} from 'jotai/utils'
+import { atom } from 'jotai'
+import { atomWithStorage, RESET } from 'jotai/utils'
 
 export function getEnv(expected: string | undefined, fallback: string) {
   if (process.env.NODE_ENV !== 'production' && expected) {
@@ -15,4 +16,15 @@ export const PARACHAIN_ENDPOINT = 'wss://poc5.phala.network/ws'
 //   'wss://poc5.phala.network/ws'
 // )
 
-export const endpointAtom = atomWithReset<string>(PARACHAIN_ENDPOINT)
+export const preferedEndpointAtom = atomWithStorage<string>('last-selected-rpc', PARACHAIN_ENDPOINT)
+
+export const endpointAtom = atom(
+  get => get(preferedEndpointAtom),
+  (get, set, next: string | typeof RESET) => {
+    if (next === RESET) {
+      set(preferedEndpointAtom, `${PARACHAIN_ENDPOINT}`)
+    } else {
+      set(preferedEndpointAtom, next)
+    }
+  }
+)
