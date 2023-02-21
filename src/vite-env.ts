7275b564679f4da5a36308b8f989e0c95a78827f
vabaly <love0and0life@gmail.com>
2023-02-21 16:59:00 +0800

    feat: change env check method

--- src/vite-env.ts
+++ src/vite-env.ts
@@ -1,2 +1,1 @@
-export const isClosedBetaEnv = import.meta.env.MODE === 'closed-beta'
-console.log('isClosedBetaEnv', isClosedBetaEnv)
+export const isClosedBetaEnv = import.meta.env.VITE_SITE_BRANCH_NAME === 'closed_beta'
