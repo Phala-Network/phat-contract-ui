96ea92815afc87062031f8f5bed30b96a187c973
lucasliu <120998498+lucasliu2022@users.noreply.github.com>
2023-03-01 16:28:16 +0800

    Allow users to select one from a pre-defined list

--- src/features/parachain/components/EndpointAddressInput.tsx
+++ src/features/parachain/components/EndpointAddressInput.tsx
@@ -1,4 +1,4 @@
-import tw from 'twin.macro'
+import tw from "twin.macro";
 import {
   FormControl,
   FormLabel,
@@ -7,21 +7,22 @@ import {
   InputRightElement,
   Button,
   ButtonGroup,
-} from '@chakra-ui/react'
-import { useAtom } from 'jotai'
-import { RESET } from 'jotai/utils'
-import { setCookie } from 'cookies-next'
+} from "@chakra-ui/react";
+import { useAtom } from "jotai";
+import { RESET } from "jotai/utils";
+import { setCookie } from "cookies-next";
 
-import { endpointAtom, PARACHAIN_ENDPOINT } from '@/atoms/endpointsAtom'
-import { websocketConnectionMachineAtom } from '@/features/parachain/atoms'
+import { endpointAtom, PARACHAIN_ENDPOINT } from "@/atoms/endpointsAtom";
+import { websocketConnectionMachineAtom } from "@/features/parachain/atoms";
+import EndpointAddressSelect from "./EndpointAddressSelect";
 
 export default function EndpointAddressInput({ label }: { label?: string }) {
-  const [endpoint, setEndpoint] = useAtom(endpointAtom)
-  const [machine, send] = useAtom(websocketConnectionMachineAtom)
+  const [endpoint, setEndpoint] = useAtom(endpointAtom);
+  const [machine, send] = useAtom(websocketConnectionMachineAtom);
   return (
     <FormControl>
-      <FormLabel>{label || 'Khala Parachain Endpoint Address'}</FormLabel>
-      <InputGroup>
+      <FormLabel>{label || "Khala Parachain Endpoint Address"}</FormLabel>
+      {/* <InputGroup>
         <Input
           type='url'
           value={endpoint}
@@ -32,10 +33,15 @@ export default function EndpointAddressInput({ label }: { label?: string }) {
             <Button
               h="1.75rem"
               size="sm"
-              disabled={!machine.can('RECONNECT') || machine.context.endpoint === endpoint}
+              disabled={
+                !machine.can("RECONNECT") ||
+                machine.context.endpoint === endpoint
+              }
               onClick={() => {
-                send({ type: 'RECONNECT', data: { endpoint } })
-                setCookie('preferred_endpoint', endpoint, { maxAge: 60 * 60 * 24 * 30 })
+                send({ type: "RECONNECT", data: { endpoint } });
+                setCookie("preferred_endpoint", endpoint, {
+                  maxAge: 60 * 60 * 24 * 30,
+                });
               }}
             >
               Connect
@@ -44,21 +50,37 @@ export default function EndpointAddressInput({ label }: { label?: string }) {
               h="1.75rem"
               size="sm"
               onClick={() => {
-                const endpoint = PARACHAIN_ENDPOINT
-                setEndpoint(RESET)
-                if (machine.can('RECONNECT')) {
-                  send({ type: 'RECONNECT', data: { endpoint } })
+                const endpoint = PARACHAIN_ENDPOINT;
+                setEndpoint(RESET);
+                if (machine.can("RECONNECT")) {
+                  send({ type: "RECONNECT", data: { endpoint } });
                 } else {
-                  send({ type: 'CONNECT', data: { endpoint } })
+                  send({ type: "CONNECT", data: { endpoint } });
                 }
-                setCookie('preferred_endpoint', endpoint, { maxAge: 60 * 60 * 24 * 30 })
+                setCookie("preferred_endpoint", endpoint, {
+                  maxAge: 60 * 60 * 24 * 30,
+                });
               }}
             >
               Reset
             </Button>
           </ButtonGroup>
         </InputRightElement>
-      </InputGroup>
+      </InputGroup> */}
+      <EndpointAddressSelect
+        value={endpoint}
+        onChange={(value) => {
+          setEndpoint(value);
+        }}
+      />
+      <div tw="mt-[8px]">
+        <Button h="1.75rem" tw="mr-[5px]" size="sm" onClick={() => {}}>
+          Connect
+        </Button>
+        <Button h="1.75rem" size="sm" onClick={() => {}}>
+          DisConnect
+        </Button>
+      </div>
     </FormControl>
-  )
+  );
 }
---
+++ src/features/parachain/components/EndpointAddressSelect.tsx
@@ -0,0 +1,22 @@
+import { Select } from "@chakra-ui/react";
+import { Atom, useAtom, WritableAtom } from "jotai";
+
+export default function EndpointAddressSelect({
+  onChange,
+  value,
+}: {
+  onChange: (s: string) => void;
+  value: string;
+}) {
+  return (
+    <Select
+      value={value}
+      onChange={(e) => {
+        onChange(e.target.value);
+      }}
+    >
+      <option>wss://poc5.phala.network/ws</option>
+      <option>wss://phat-beta.phala.network/khala/ws</option>
+    </Select>
+  );
+}
