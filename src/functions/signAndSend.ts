a61f75944574bbd1b00f2484261fe470655e38eb
Leechael Yim <yanleech@gmail.com>
2023-06-24 17:11:37 +0800

    fix: the signAndSend helper haven't handle user cancelled event.

--- src/functions/signAndSend.ts
+++ src/functions/signAndSend.ts
@@ -3,36 +3,40 @@ import type { SubmittableExtrinsic, SignerOptions } from '@polkadot/api-base/typ
 
 function signAndSend<ApiType extends ApiTypes>(target: SubmittableExtrinsic<ApiType>, address: string, signer: SignerOptions['signer']) {
   return new Promise(async (resolve, reject) => {
-    // Ready -> Broadcast -> InBlock -> Finalized
-    const unsub = await target.signAndSend(
-      address, { signer }, (result) => {
-        if (result.status.isInBlock) {
-          let error;
-          for (const e of result.events) {
-            const { event: { data, method, section } } = e;
-            if (section === 'system' && method === 'ExtrinsicFailed') {
-              error = data[0];
+    try {
+      // Ready -> Broadcast -> InBlock -> Finalized
+      const unsub = await target.signAndSend(
+        address, { signer }, (result) => {
+          if (result.status.isInBlock) {
+            let error;
+            for (const e of result.events) {
+              const { event: { data, method, section } } = e;
+              if (section === 'system' && method === 'ExtrinsicFailed') {
+                error = data[0];
+              }
             }
+            // @ts-ignore
+            unsub();
+            if (error) {
+              reject(error);
+            } else {
+              resolve({
+                hash: result.status.asInBlock.toHuman(),
+                // @ts-ignore
+                events: result.toHuman().events,
+                result,
+              });
+            }
+          } else if (result.status.isInvalid) {
+            // @ts-ignore
+            unsub();
+            reject('Invalid transaction');
           }
-          // @ts-ignore
-          unsub();
-          if (error) {
-            reject(error);
-          } else {
-            resolve({
-              hash: result.status.asInBlock.toHuman(),
-              // @ts-ignore
-              events: result.toHuman().events,
-              result,
-            });
-          }
-        } else if (result.status.isInvalid) {
-          // @ts-ignore
-          unsub();
-          reject('Invalid transaction');
         }
-      }
-    )
+      )
+    } catch (err) {
+      reject(err)
+    }
   })
 }
 
