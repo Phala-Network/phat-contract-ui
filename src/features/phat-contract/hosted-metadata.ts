fc7fbf92997e2cbb87997743bf590f8078bb225b
Leechael Yim <yanleech@gmail.com>
2023-07-28 21:45:23 +0800

    refactor: no extra library dependencies for hosted metadata fetchers & more.

--- src/features/phat-contract/hosted-metadata.ts
+++ src/features/phat-contract/hosted-metadata.ts
@@ -1,6 +1,7 @@
 import { type OnChainRegistry, type PinkContractPromise, type CertificateData } from '@phala/sdk'
 import { type Bool } from '@polkadot/types'
-import * as R from 'ramda'
+
+const OFFICIAL_ARTIFACTS_URL = 'https://phala-network.github.io/phat-contract-artifacts'
 
 export interface CheckCodeHashExistsEnv {
   systemContract: PinkContractPromise
@@ -17,8 +18,7 @@ export function unsafeCheckCodeHashExists(env: CheckCodeHashExistsEnv) {
 
 export async function unsafeGetContractCodeHash(phatRegistry: OnChainRegistry, contractId: string): Promise<string | null> {
   const payload = await phatRegistry.phactory.getContractInfo({ contracts: [contractId] })
-  const codeHash = R.path(['contracts', 0, 'codeHash'], payload)
-  return codeHash || null
+  return payload?.contracts[0]?.codeHash || null
 }
 
 export async function unsafeGetAbiFromPatronByCodeHash(codeHash: string): Promise<Record<string, unknown>> {
@@ -36,14 +36,14 @@ export async function unsafeGetAbiFromPatronByCodeHash(codeHash: string): Promis
         throw new Error(`Unknown Error: ${resp.status}: ${_err2}`)
       }
     }
-    throw new Error(`Failed to get abi from Patron: ${resp.status}: ${R.propOr('Unknown Error', 'error', payload)}`)
+    throw new Error(`Failed to get abi from Patron: ${resp.status}: ${payload?.error || 'Unknown Error'}`)
   }
   return await resp.json()
 }
 
 export async function unsafeGetAbiFromGitHubRepoByCodeHash(codeHash: string): Promise<Record<string, unknown>> {
   const codeHashWithPrefix = codeHash.indexOf('0x') !== 0 ? `0x${codeHash}` : codeHash
-  const resp = await fetch(`https://leechael.github.io/phat-contract-artifacts/artifacts/${codeHashWithPrefix}/metadata.json`)
+  const resp = await fetch(`${OFFICIAL_ARTIFACTS_URL}/artifacts/${codeHashWithPrefix}/metadata.json`)
   if (resp.status !== 200) {
     throw new Error(`Failed to get abi from GitHub: ${resp.status}`)
   }
@@ -54,7 +54,7 @@ export async function unsafeGetWasmFromPatronByCodeHash(codeHash: string): Promi
   const codeHashWithoutPrefix = codeHash.indexOf('0x') === 0 ? codeHash.replace('0x', '') : codeHash
   const resp = await fetch(`https://api.patron.works/buildSessions/wasm/${codeHashWithoutPrefix}`)
   if (resp.status !== 200) {
-    throw new Error(`Failed to get abi from Patron: ${resp.status}`)
+    throw new Error(`Failed to get wasm from Patron: ${resp.status}`)
   }
   const buffer = await resp.arrayBuffer()
   return new Uint8Array(buffer)
