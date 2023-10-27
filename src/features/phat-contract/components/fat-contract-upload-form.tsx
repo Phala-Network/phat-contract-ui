03d2c4ba737d95ff1a2cfb6d6527ba66a2114032
pacoyang <yangbaigao@gmail.com>
2023-10-27 11:02:27 +0800

    fix: select transfer account based on endpoint

--- src/features/phat-contract/components/fat-contract-upload-form.tsx
+++ src/features/phat-contract/components/fat-contract-upload-form.tsx
@@ -769,6 +769,7 @@ function InstantiateInfoCandidateHint() {
 
 function GetPhaButton() {
   const api = useAtomValue(apiPromiseAtom)
+  const endpoint = useAtomValue(endpointAtom)
   const isDevChain = useAtomValue(isDevChainAtom)
   const account = useAtomValue(currentAccountAtom)
   const [loading, setLoading] = useState(false)
@@ -784,7 +785,11 @@ function GetPhaButton() {
   async function getTestCoin () {
     setLoading(true)
     const keyring = new Keyring({ type: 'sr25519' })
-    const pair = keyring.addFromUri('purchase issue dinner sock coin brown buddy vehicle clock insect traffic sting')
+    let suri = '//Alice'
+    if (endpoint.startsWith('wss://poc6.phala.network')) {
+      suri = 'purchase issue dinner sock coin brown buddy vehicle clock insect traffic sting'
+    }
+    const pair = keyring.addFromUri(suri)
     await api.tx.balances.transferKeepAlive(account?.address, '1000000000000000')
       .signAndSend(pair, { nonce: -1 })
     setLoading(false)
